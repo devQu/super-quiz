4c44247ae13b40af4b4ffb36d8bfe0c6829716cf
devQu <unreal-tabby@ya.ru>
2020-12-03 08:57:34 +0300

    sc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Layout from './hoc/Layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import QuizList from './containers/QuizList/QuizList';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import Auth from './containers/Auth/Auth';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/quiz/:id" component={Quiz} ></Route>
+        <Route path="/quiz-create" component={QuizCreator} ></Route>
+        <Route path="/auth" component={Auth} ></Route>
+        <Route path="/" component={QuizList} ></Route>
+      </Switch>
     </Layout>
   );
 }
